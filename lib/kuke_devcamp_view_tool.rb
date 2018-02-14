b72f0dadd1f0032bd30f17066442f03ead4816ef
kuke <j.steinstraeter@gmail.com>
2018-02-14 10:46:04 +0100

    init

---
+++ lib/kuke_devcamp_view_tool.rb
@@ -0,0 +1,9 @@
+require "kuke_devcamp_view_tool/version"
+
+module KukeDevcampViewTool
+  class Renderer
+    def self.copyright name, msg
+      "&copy; #{Time.now.year} <b>#{name}<b> #{msg}".html_safe
+    end
+  end
+end
